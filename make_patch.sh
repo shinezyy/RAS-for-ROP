0ade7dd06fa97da72038de9e06c065aa7c0f81df
Zhou Yaoyang <shinezyy@qq.com>
2018-05-05 18:24:57 +0800

    make patch

---
+++ make_patch.sh
@@ -0,0 +1,5 @@
+git diff -u 679ff8 cpu-exec.c > cpu-exec_modify.patch
+git diff -u 679ff8 include/exec/exec-all.h > ./exec-all_modify.patch
+git diff -u 679ff8 include/exec/tb-context.h > ./tb-context_modify.patch
+git diff -u 679ff8 linux-user/main.c > ./main_modify.patch
+git diff -u 679ff8 target-i386/translate.c > ./translate_modify.patch
